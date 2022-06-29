ac26a4967e1125148dfc13dfba98c909d8ebf498
ManiRmp <maniramznpoor2007@gmail.com>
2022-06-29 23:50:57 +0430

    added Cart Value Src

---
+++ js/Products.js
@@ -0,0 +1,10 @@
+export const ProductsData = [
+  { id: 1, price: 14.42, title: "Product1", imgUrl: "./img/product1.jpg" },
+  { id: 2, price: 14.42, title: "Product2", imgUrl: "./img/product2.jpg" },
+  { id: 3, price: 14.42, title: "Product3", imgUrl: "./img/product3.jpg" },
+  { id: 4, price: 14.42, title: "Product4", imgUrl: "./img/product4.jpg" },
+  { id: 5, price: 14.42, title: "Product5", imgUrl: "./img/product5.jpg" },
+  { id: 6, price: 14.42, title: "Product6", imgUrl: "./img/product6.jpg" },
+  { id: 7, price: 14.42, title: "Product7", imgUrl: "./img/product7.jpg" },
+  { id: 8, price: 14.42, title: "Product8", imgUrl: "./img/product8.jpg" },
+];
